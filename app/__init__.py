ee986434641e02921fd498bf70edf357f6ae5318
LP <lp.passarelli.wilson@gmail.com>
2022-10-20 14:20:06 -0700

    got it working so that a GET request to /journal gives a list of journals

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask  #not sure why this isn't reading.  maybe because anothe
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+    #make sure Flask knows app functions exist
     from .routes.journal import journal_bp
+    app.register_blueprint(journal_bp)
     return app
--- app/routes/journal.py
+++ app/routes/journal.py
@@ -1,6 +1,5 @@
 
-from flask import Blueprint
-#import jsonify
+from flask import Blueprint, jsonify
 
 class Journal:
     def __init__(self, id, design, dye_color = "canyon tan", size = "A6", design_details = None):
@@ -25,4 +24,15 @@ journal_bp = Blueprint("journal_bp" , __name__, url_prefix = "/journal")
 #we have a little tag on this to tell flask when to do this function:
 @journal_bp.route("", methods = ["GET"])   #"" will be added to the end of the URL.  it's empty, so this route will handle things for /bike.  the method argument must be a list. 
 def get_all_journals():
-    return jsonify(["this", "is", "working"]), 200  #this is the status code that will come back. 
+    response = []
+    for journal in journals:
+        journal_dict = {
+            "id" : journal.id,
+            "design" : journal.design,
+            "dye color": journal.dye_color,
+            "size": journal.size,
+            "design_details": journal.design_details
+        }
+        response.append(journal_dict)
+    
+    return jsonify(response), 200  #this is the status code that will come back. 
