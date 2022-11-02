8fd49802a74c738733d491da793326a4fbb31c40
LP <lp.passarelli.wilson@gmail.com>
2022-11-01 21:34:37 -0700

    setting up test functionality

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ def create_app(testing = None):
     if testing is None:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     else:
+        app.config["TESTING"] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     #you have to import this before db.init_app(app)
